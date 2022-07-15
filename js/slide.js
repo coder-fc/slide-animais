3d61906f807e285498fed86e909f9da75fea42a9
Felipe Costa <felipech3344@gmail.com>
2022-07-14 23:31:04 -0300

    Realizados os calculos de distancia entre os slides e adicionados os eventos aos slide

--- js/slide.js
+++ js/slide.js
@@ -1,20 +1,36 @@
 export default class Slide {
     constructor(slide, container) {
-        this.slide = document.querySelector(slide)
-        this.container = document.querySelector(container)
+        this.slide = document.querySelector(slide);
+        this.container = document.querySelector(container);
+        this.dist = {
+            finalPosition: 0, startX: 0, movement:0
+        }
+    }
+
+    moveSlide(distX) {
+        this.dist.movePosition = distX
+        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
+    }
+
+    uptadePosition(clientX) {
+        this.dist.movement = (this.dist.startX - clientX * 1.4);
+        return this.dist.finalPosition - this.dist.movement;
     }
 
     onStart(event) {
         event.preventDefault();
+        this.dist.startX = event.clientX;
         this.container.addEventListener('mousemove', this.onMove);
     }
 
     onMove(event) {
-
+        const finalPosition = this.uptadePosition(event.clientX);
+        this.moveSlide(finalPosition);
     }
 
     onEnd(event) {
         this.container.removeEventListener('mousemove', this.onMove)
+        this.dist.finalPosition = this.dist.movePosition;
     }
 
     addEventsSlide() {
